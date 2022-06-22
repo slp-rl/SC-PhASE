1a00a710ca99a030fdc9ce67c8913381f5d52871
ortal1602 <or.tal1@mail.huji.ac.il>
2022-06-22 13:56:09 +0300

    init commit - added all project files

--- utils/visualization.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-
-def convert_spectrogram_to_heatmap(spectrogram):
-    spectrogram = (255 * (spectrogram - np.min(spectrogram)) / np.ptp(spectrogram)).astype(np.uint8).squeeze()
-    heatmap = cv2.applyColorMap(spectrogram, cv2.COLORMAP_INFERNO)
-    heatmap = cv2.cvtColor(heatmap, cv2.COLOR_BGR2RGB)
-    return heatmap
